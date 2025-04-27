3854acb3aac4a14ff6b9c66da1dbccde313e667d
etradingmanager <etradingmanager@gmail.com>
2025-04-27 12:04:01 +1000

    Update fetch-tyres.js

--- api/fetch-tyres.js
+++ api/fetch-tyres.js
@@ -11,8 +11,11 @@ export default async function handler(req, res) {
     });
     const data = await response.json();
 
-    // ✨ CORS fix: Add correct header
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    // CORS fix
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS'); 
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
     res.status(200).json(data);
 
   } catch (error) {
