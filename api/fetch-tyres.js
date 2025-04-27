01ab3fe0a8f4c7d2865e0d14852807a759820de9
etradingmanager <etradingmanager@gmail.com>
2025-04-27 11:48:47 +1000

    Update fetch-tyres.js

--- api/fetch-tyres.js
+++ api/fetch-tyres.js
@@ -1,10 +1,14 @@
 export default async function handler(req, res) {
   const storeId = '86413255';
   const token = 'public_TyrqEQkEXtqK5u27fvLwS69RURXdfSqt';
-  const apiUrl = `https://app.ecwid.com/api/v3/${storeId}/products?token=${token}&limit=1000`;
+  const apiUrl = `https://app.ecwid.com/api/v3/${storeId}/products?limit=1000`;
 
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiUrl, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
